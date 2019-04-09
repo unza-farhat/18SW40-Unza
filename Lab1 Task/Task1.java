a82032c2834ae50e005aaf24f7a68c96143c5bbe
unza-farhat <49427250+unza-farhat@users.noreply.github.com>
2019-04-09 12:44:17 +0500

    Add files via upload

---
+++ Lab1 Task/Task1.java
@@ -0,0 +1,19 @@
+class Task1{
+    public static void main(String [] args)
+	{
+	String name="Unza Farhat";
+	String rollno="18SW40";
+	int TotalMark=100;
+	float ObtMark=87f;
+	float per=(ObtMark/TotalMark)*100;
+	char G='A';
+	boolean s=true;
+	System.out.println("Name: " +name);
+	System.out.println("Roll no" +rollno);
+	System.out.println("Total Marks: " +TotalMark);
+	System.out.println("Obtained Marks: " +ObtMark);
+	System.out.println("Percentage: " +per);
+	System.out.println("Grade: " +G);
+	System.out.println("Status: " +s);
+	}
+}
---
+++ Lab1 Task/Task2.java
@@ -0,0 +1,12 @@
+class Task2
+{
+    public static void main(String [] args)
+	{
+	double a=50.25;
+	long b=(int)a;
+	double c=a-b;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	}
+}
